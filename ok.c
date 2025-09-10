1694c4abd1daf6015f8ce47b33bfb16a54f17dce
prayasp18 <pp4721@srmist.edu.in>
2025-09-10 22:00:25 +0530

    Removed ok.c from repository

--- ok.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    char c = 'A', C = 'a';
-    int result = c++ * ++C;   // ASCII values are multiplied
-    printf("c = %c (%d)\n", c, c);
-    printf("C = %c (%d)\n", C, C);
-    printf("Result = %d\n", result);
-    return 0;
-}
